06cbb12464a2ca3d3c2f6d90946359ab6c06fc0f
Nandana <83871623+NandanaBalaram@users.noreply.github.com>
2021-08-01 14:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let url = "https://finalspaceapi.com/api/v0/character"
 
-async function getData(){
+async function getData() {
+  try {
     const result = fetch(url)
     const data = await (await result).json()
     console.log(data)
     data.forEach(element => {
-        const character = document.getElementsByClassName('card-deck')
-        character[0].innerHTML += `<div class="card mb-5" style="min-width: 15rem;">
+      const character = document.getElementsByClassName('card-deck')
+      character[0].innerHTML += `<div class="card mb-5" style="min-width: 15rem;">
         <img src="${element.img_url}" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">${element.name}</h5>
           <p class="card-text">Origin: ${element.origin} <br> Species: ${element.species}</p>
           <p class="card-text"><small class="text-dark">Abilities: ${element.abilities.toString()}</small></p>
         </div>
-      </div>` 
+      </div>`
     });
+  }
+  catch (err) {
+    console.log(err)
+  }
 }
